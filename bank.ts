39abc0435fb000e4ec578dd77ae3ca6f972117c8
Daniel Cambinda <danielcambinda@gmail.com>
2023-04-07 20:58:09 +0100

    test on mac

--- bank.ts
+++ bank.ts
@@ -1,69 +1,127 @@
-import * as yargs from 'yargs'
-import path from 'path'
-import net from 'net'
-import * as fs from 'node:fs/promises'
+import * as yargs from "yargs";
+import path from "path";
+import net from "net";
+import { existsSync } from "node:fs";
+import * as fs from "node:fs/promises";
 
+let users = new Set<{}>();
 
-//get arguments from argv
-const args = yargs.options({
-    p:{
-        describe:'bank server port',
-        type: 'number',
-    },
-    s: {
-        describe: 'Name of the auth file',
-        type:"string"
-    }
-  }).check( (argv) : true | void =>  {
+type StoreRequest = {
+  store: {
+    operations: "get_valid";
+    data: {
+      name_auth_file: string;
+      vcc: string;
+      shopping_value: number;
+    };
+  };
+};
 
-    const argvKeys = Object.keys(argv)
-    const permitedKeys = ['p','s','_','$0']
-    const validArgs = argvKeys.every( key => permitedKeys.includes(key))
+type MBECRequest = {};
 
-    console.log("validArgs: ", validArgs)
-
-    // if there are any invalid arguments
-    if (!validArgs){
-        console.log("invalid args")
-        return process.exit(125)
-    }
-
-    //p can not be provided
-    // if there are duplicate p arguments p is an array
-    if ( argv.p && Array.isArray(argv.p) ){
-        console.log("invalid p")
-       return process.exit(125) 
-    }
-    //s can not be provided
-    // if there are duplicate s arguments s is an array 
-    if ( argv.s && Array.isArray(argv.s) ){
-        console.log("invalid s")
-        return process.exit(125) 
-    }
-
-    return true
-  }).parseSync()
-
-// get the port number and the auth file name
-let { p, s } = args
-
-p == undefined ? p = 3000 : p = p // if p is not provided, use 3000 as default
-s == undefined ? s = 'bank.auth' : s = s // if s is not provided, use 'bank.auth' as default
-
-//server excution
-const main = async (p: number, s: string) =>  {
-    const server = net.createServer(async (socket) => {
-
-        socket.on('data', (data) => { // where I'm going to receive the data from the client
-            const message = data.toString().split(' ')
-            console.log(JSON.stringify({message}))
-        })
-
-    })
-
-    server.listen(p, () => {
-        console.log(`Server is listening on port ${p}`) 
-    })
+function generateRandomString(length: number): string {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-=_+[]{}|;:,.<>?/";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
 
-main(p,s)
+//server excution
+const main = async () => {
+  try {
+    //get arguments from argv
+    const args = yargs
+      .options({
+        p: {
+          describe: "bank server port",
+          type: "number",
+        },
+        s: {
+          describe: "Name of the auth file",
+          type: "string",
+        },
+      })
+      .check((argv): true | void => {
+        const argvKeys = Object.keys(argv);
+        const permitedKeys = ["p", "s", "_", "$0"];
+        const validArgs = argvKeys.every((key) => permitedKeys.includes(key));
+
+        console.log("validArgs: ", validArgs);
+
+        // if there are any invalid arguments
+        if (!validArgs) {
+          console.log("invalid args");
+          return process.exit(125);
+        }
+
+        //p can not be provided
+        // if there are duplicate p arguments p is an array
+        if (argv.p && Array.isArray(argv.p)) {
+          console.log("invalid p");
+          return process.exit(125);
+        }
+        //s can not be provided
+        // if there are duplicate s arguments s is an array
+        if (argv.s && Array.isArray(argv.s)) {
+          console.log("invalid s");
+          return process.exit(125);
+        }
+
+        return true;
+      })
+      .parseAsync();
+
+    // get the port number and the auth file name
+    let { p, s } = await args;
+
+    p == undefined ? (p = 3000) : (p = p); // if p is not provided, use 3000 as default
+    s == undefined ? (s = "bank.auth") : (s = s); // if s is not provided, use 'bank.auth' as default
+
+    // check/create the auth file
+    const authFilePath = path.join(__dirname, s);
+    const authFileExists = existsSync(authFilePath);
+    if (authFileExists) {
+      //console.log("auth file already exists");
+      return process.exit(125);
+    }
+
+    // create the auth file
+    fs.writeFile(authFilePath, generateRandomString(128)).then(() => {
+      console.log("created");
+    }).catch((err) => console.error(err));
+
+    // create the server
+    const server: net.Server = net.createServer(async (socket) => {
+
+      socket.on("data", (data: Buffer) => {
+        // where I'm going to receive the data from the client
+        const data_content = JSON.parse(data.toString());
+
+        if(data_content.store){
+            const store_content = data_content as StoreRequest;
+            console.log(store_content);
+            return socket.write('store content detected');
+        }
+        
+      });
+
+      socket.on('connec', () => {
+            console.log('connected');
+      })
+
+
+    });
+
+    server.listen(p, () => {
+      console.log(`Server is listening on port ${p}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+main();
